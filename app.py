a0f1375fee5429bb159c7a4b2da9d8647da47ed1
Nishant Kumar <nniishantkumar@gmail.com>
2023-01-22 13:34:07 +0530

    Cleaning up

--- app.py
+++ app.py
@@ -8,20 +8,6 @@ from driver_engine import Engine
 def show_results(results):
     st.markdown(results.to_html(render_links=True, escape=False),unsafe_allow_html=True)
 
-    # for x, email in enumerate(results['name']):
-    #     col1, col2, col3, col4, col5 = st.columns((1, 2, 2, 1, 1))
-    #     col1.write(x)  # index
-    #     col2.write(results['name'][x])  # email
-    #     col3.write(results['size'][x])  # unique ID
-    #     col4.write(results['seeds'][x])   # email status
-    #     # disable_status = results['disabled'][x]  # flexible type of button
-    #     # button_type = "Unblock" if disable_status else "Block"
-    #     button_phold = col5.empty()  # create a placeholder
-    #     do_action = button_phold.button(button_type, key=x)
-    #     if do_action:
-    #          pass # do some action with row's data
-    #          button_phold.empty()  #  remove button
-
 
 def main():
     st.title("Movie search engine")
--- driver_engine.py
+++ driver_engine.py
@@ -18,8 +18,8 @@ class Engine():
                 results.append(val)
             df = pd.DataFrame.from_dict(results)
             df['size'] = (pd.to_numeric(df['size'].str[:-2])/1024/1024/1024).astype(float).round(2).astype(str) + " GB"
-            df['magnet'] = df['link'].apply(lambda x: f'<a target="_blank" href="{x}">Download</a>')
-            df = df[['name', 'size', 'seeds', 'leech', 'magnet']]
+            df['link'] = df['link'].apply(lambda x: f'<a target="_blank" href="{x}">Download</a>')
+            df = df[['name', 'size', 'seeds', 'leech', 'link']]
             df = df.astype({'seeds': 'int32', 'leech': 'int32'}, copy=False)
             df.sort_values(by=['seeds'], ascending=False, inplace=True, ignore_index=True)
 
